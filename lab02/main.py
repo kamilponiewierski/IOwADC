32f7e631d14c5a8cb5c0067d8863bf60c2fe3348
Kamil Poniewierski <kamiljanponiewierski@gmail.com>
2025-03-27 20:39:52 +0100

    Add third problem

--- lab02/main.py
+++ lab02/main.py
@@ -1,12 +1,22 @@
+from enum import Enum
 from searchMPP import SearcherMPP
 from stripsForwardPlanner import Forward_STRIPS
 from stripsProblem import Planning_problem, STRIPS_domain, Strips
 
+
+class Building(Enum):
+    EMPTY = ""
+    BARRACKS = "Barracks"
+    DEPOT = "Depot"
+
+
 # minerals = {"mineral_field_a"}
 sectors = set(map(lambda x: f"Sector_{x}", range(1, 3)))
 boolean = {True, False}
-building = {"", "Barracks", "Depot"}
+building = set(e.value for e in Building)
+units = {"", "Marine", "Wraith", "Tank"}
 mineralField = {"", "Minerals"}
+mineral_fields = set(map(lambda x: f"Minerals_{x}", range(1, 4)))
 
 
 collect_minerals_actions = list(
@@ -23,14 +33,12 @@ collect_minerals_actions = list(
                 f"Minerals_{x}": "",
             },
         ),
-        range(1, 3),
+        range(1, len(mineral_fields) + 1),
     )
 )
 
 move_scv_actions = []
 
-
-mineral_fields = set(map(lambda x: f"Minerals_{x}", range(1, 3)))
 locations = mineral_fields | sectors
 
 for l_1 in locations:
@@ -44,21 +52,62 @@ for l_1 in locations:
                 )
             )
 
-build_depot_actions = list(map(
-    lambda x: Strips(
-        f"Build_Depot_{x}",
-        {"SCV_location": f"Sector_{x}", f"Sector_{x}": "", "HasMinerals": True},
-        {f"Sector_{x}": "Depot", "HasMinerals": False},
-    ),
-    range(1, len(sectors) + 1),
-))
+build_depot_actions = list(
+    map(
+        lambda x: Strips(
+            f"Build_Depot_{x}",
+            {
+                "SCV_location": f"Sector_{x}",
+                f"Sector_{x}": Building.EMPTY,
+                "HasMinerals": True,
+            },
+            {f"Sector_{x}": Building.DEPOT, "HasMinerals": False},
+        ),
+        range(1, len(sectors) + 1),
+    )
+)
 
-units = {'', 'Marine', 'Wraith', 'Tank'}
+build_barracks_actions = []
+for i in range(1, len(sectors) + 1):
+    for j in range(1, len(sectors) + 1):
+        if i != j:
+            build_barracks_actions.append(
+                Strips(
+                    f"Build_Barracks_{i}_With_Depot_At_{j}",
+                    {
+                        "HasMinerals": True,
+                        f"Sector_{i}": Building.EMPTY,
+                        "SCV_location": f"Sector_{i}",
+                        f"Sector_{j}": Building.DEPOT,
+                    },
+                    {"HasMinerals": False, f"Sector_{i}": Building.BARRACKS},
+                )
+            )
+
+
+train_marine_actions = list(
+    map(
+        lambda x: Strips(
+            f"train_marine_{x}",
+            {
+                f"Sector_{x}": Building.BARRACKS,
+                "HasMinerals": True,
+                f"Unit_{x}": "",
+            },
+            {
+                f"Unit_{x}": "Marine",
+                "HasMinerals": False,
+            },
+        ),
+        range(1, len(sectors) + 1),
+    )
+)
 
 starcraft_domain = STRIPS_domain(
     feature_domain_dict={
         "Minerals_1": mineralField,
         "Minerals_2": mineralField,
+        "Minerals_3": mineralField,
         "Sector_1": building,
         "Sector_2": building,
         "HasMinerals": boolean,
@@ -71,57 +120,63 @@ starcraft_domain = STRIPS_domain(
         *collect_minerals_actions,
         *move_scv_actions,
         *build_depot_actions,
+        *build_barracks_actions,
+        *train_marine_actions,
         Strips(
             "Build_Barracks_1",
             {
                 "HasMinerals": True,
-                "Sector_1": "",
+                "Sector_1": Building.EMPTY,
                 "SCV_location": "Sector_1",
-                "Sector_2": "Depot",
+                "Sector_2": Building.DEPOT,
             },
-            {"HasMinerals": False, "Sector_1": "Barracks"},
+            {"HasMinerals": False, "Sector_1": Building.BARRACKS},
         ),
     },
 )
 
 initial_units_state = {
-    "Unit_1": '',
-    "Unit_2": '',
-    "Unit_3": '',
+    "Unit_1": "",
+    "Unit_2": "",
+    "Unit_3": "",
 }
 
 initial_sector_state = {
-    "Sector_1": "",
-    "Sector_2": "",
+    "Sector_1": Building.EMPTY,
+    "Sector_2": Building.EMPTY,
 }
 
 initial_minerals_state = {
     "HasMinerals": False,
     "Minerals_1": "Minerals",
     "Minerals_2": "Minerals",
+    "Minerals_3": "Minerals",
+}
+
+initial_state = {
+    **initial_minerals_state,
+    **initial_sector_state,
+    "SCV_location": "Sector_1",
+    **initial_units_state,
 }
 
 build_barracks_problem = Planning_problem(
     prob_domain=starcraft_domain,
-    initial_state={
-        **initial_minerals_state,
-        **initial_sector_state,
-        "SCV_location": "Sector_1",
-        **initial_units_state
-    },
-    goal={"Sector_1": "Barracks"},
+    initial_state=initial_state,
+    goal={"Sector_1": Building.BARRACKS},
 )
 
 build_depot_problem = Planning_problem(
     prob_domain=starcraft_domain,
-    initial_state={
-        **initial_minerals_state,
-        **initial_sector_state,
-        "SCV_location": "Sector_1",
-        **initial_units_state
-    },
-    goal={"Sector_1": "Depot"},
+    initial_state=initial_state,
+    goal={"Sector_1": Building.DEPOT},
 )
 
-s1 = SearcherMPP(Forward_STRIPS(build_depot_problem))  # A*
+train_marine_problem = Planning_problem(
+    prob_domain=starcraft_domain,
+    initial_state=initial_state,
+    goal={"Unit_1": "Marine"},
+)
+
+s1 = SearcherMPP(Forward_STRIPS(train_marine_problem))  # A*
 s1.search()  # find another plan
