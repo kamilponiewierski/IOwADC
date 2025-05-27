1fd697b6b9e138804b40aa81474e224872cccd06
Kamil Poniewierski <kamilponiewierski98@gmail.com>
2025-05-27 19:26:58 +0200

    remove unused files, improve simple render

--- lab05/simple_render.py
+++ lab05/simple_render.py
@@ -1,16 +1,26 @@
 import gymnasium as gym
 from stable_baselines3 import TD3
+
 if __name__ == "__main__":
-    env = gym.make("LunarLanderContinuous-v3", render_mode="human")
-    model = TD3.load('tmp/Large Network_run_0/final_model', env=env, device='cpu')
-    obs, _ = env.reset()
-    done = False
-    rewards = 0
-    while not done:
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
-        done = terminated or truncated
-        env.render()
-        rewards += reward
+    rewards = []
+
+    for i in range(40):
+        env = gym.make(
+            "LunarLanderContinuous-v3",
+            # render_mode="human",
+        )
+        model = TD3.load("tmp/Large Network_run_0/final_model", env=env, device="cpu")
+        obs, _ = env.reset()
+        done = False
+        current_rewards = 0
+        while not done:
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            env.render()
+            current_rewards += reward
         
-    print(f"Total rewards: {rewards}")
+        rewards.append(current_rewards)
+    
+    print(f"Average reward over 40 episodes: {sum(rewards) / len(rewards)}")
+    print(f"Median reward over 40 episodes: {sorted(rewards)[len(rewards) // 2]}")
